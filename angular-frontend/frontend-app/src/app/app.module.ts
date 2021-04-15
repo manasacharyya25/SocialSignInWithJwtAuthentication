dfa0aac9b7f17efe9e049ea3e52b27e757e1707b
Manas Acharyya <manas.acharyya.nits.cse18@gmail.com>
2021-04-15 17:59:03 +0530

    Import SocialLoginProvider in AppModule.ts and configure Client Ids

--- angular-frontend/frontend-app/src/app/app.module.ts
+++ angular-frontend/frontend-app/src/app/app.module.ts
@@ -6,6 +6,13 @@ import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { ProfileComponent } from './profile/profile.component';
 
+import { SocialLoginModule, SocialAuthServiceConfig } from 'angularx-social-login';
+import {
+  GoogleLoginProvider,
+  FacebookLoginProvider
+} from 'angularx-social-login';
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -16,7 +23,26 @@ import { ProfileComponent } from './profile/profile.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '735847303474-ghpp9lj0dukt8583c8fva6c27p6c14tj.apps.googleusercontent.com'
+            )
+          }
+          // {
+          //   id: FacebookLoginProvider.PROVIDER_ID,
+          //   provider: new FacebookLoginProvider('clientId')
+          // }
+        ]
+      } as SocialAuthServiceConfig,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
